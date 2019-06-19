0669075785a1dad4b8c31b19da0b92760db2bb13
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2019-06-19 14:23:46 +0200

    Add validators to proper password, dateOfBirth and email form. Add information in case invalid registration data.

--- src/main/java/com/example/library/controller/UserRegistrationController.java
+++ src/main/java/com/example/library/controller/UserRegistrationController.java
@@ -2,58 +2,68 @@ package com.example.library.controller;
 
 
 import com.example.library.dao.UserRepository;
-import com.example.library.domain.Book;
 import com.example.library.domain.User;
+import com.example.library.utils.UserDataValidators;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @Controller
 public class UserRegistrationController {
 
     private UserRepository repository;
+    private UserDataValidators validators;
 
     @Autowired
-    public UserRegistrationController(UserRepository repository) {
+    public UserRegistrationController(UserRepository repository, UserDataValidators validators) {
         this.repository = repository;
+        this.validators = validators;
     }
 
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
     public String saveNewUser(Model model
-    ,@RequestParam(value = "login") String login
-    ,@RequestParam(value = "password") String password
-    ,@RequestParam(value = "email") String email
-    ,@RequestParam(value = "firstName") String firstName
-    ,@RequestParam(value = "lastName") String lastName
-    ,@RequestParam(value = "phoneNumber") Long phoneNumber
-    ,@RequestParam(value = "dateOfBirth") String dateOfBirth
-    ,@RequestParam(value = "address") String address
-    ,@RequestParam(value = "city") String city
-    ,@RequestParam(value = "zipCode") String zipCode) {
+            , @RequestParam(value = "login") String login
+            , @RequestParam(value = "password") String password
+            , @RequestParam(value = "email") String email
+            , @RequestParam(value = "firstName") String firstName
+            , @RequestParam(value = "lastName") String lastName
+            , @RequestParam(value = "phoneNumber") Long phoneNumber
+            , @RequestParam(value = "dateOfBirth") String dateOfBirth
+            , @RequestParam(value = "address") String address
+            , @RequestParam(value = "city") String city
+            , @RequestParam(value = "zipCode") String zipCode) {
 
-        User user = new User.UserBuilder()
-                .setUserLogin(login)
-                .setUserPassword(password)
-                .setUserEmail(email)
-                .setUserPhoneNumber(phoneNumber)
-                .setUserFirstName(firstName)
-                .setUserLastName(lastName)
-                .setUserDateOfBirth(dateOfBirth)
-                .setUserAddress(address)
-                .setUserCity(city)
-                .setUserZipCode(zipCode)
-                .build();
+        User user = getUser(login, password, email, firstName, lastName, phoneNumber, dateOfBirth, address, city, zipCode);
 
-        repository.save(user);
+        boolean areRegistrationDataCorrect = validators.validateUserData(user);
 
-        return "userSave";
+        if (areRegistrationDataCorrect) {
+            repository.save(user);
+            return "userSave";
+        } else {
+            model.addAttribute("IncorrectData", true);
+            return "addUser";
+        }
+
     }
+
+    private User getUser(@RequestParam("login") String login, @RequestParam("password") String password, @RequestParam("email") String email, @RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName, @RequestParam("phoneNumber") Long phoneNumber, @RequestParam("dateOfBirth") String dateOfBirth, @RequestParam("address") String address, @RequestParam("city") String city, @RequestParam("zipCode") String zipCode) {
+        return new User.UserBuilder()
+                    .setUserLogin(login)
+                    .setUserPassword(password)
+                    .setUserEmail(email)
+                    .setUserPhoneNumber(phoneNumber)
+                    .setUserFirstName(firstName)
+                    .setUserLastName(lastName)
+                    .setUserDateOfBirth(dateOfBirth)
+                    .setUserAddress(address)
+                    .setUserCity(city)
+                    .setUserZipCode(zipCode)
+                    .build();
+    }
+
 
 }
