5add25f8369a729a4ac422f96df2aca9e9b76340
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2019-06-19 11:12:17 +0200

    add simple form for user registration, add user views controller, ad basic html pages for menu

---
+++ src/main/java/com/example/library/controller/UserViewsController.java
@@ -0,0 +1,19 @@
+package com.example.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserViewsController {
+
+    @RequestMapping("/userOptions")
+    public String userOptions() {
+        return "userOptions";
+    }
+
+    @RequestMapping("/addUser")
+    public String addUser() {
+        return "addUser";
+    }
+
+}
